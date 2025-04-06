4d247bfbfd02373b72474186bb29d32fa2d841f5
Blaž Kristan <blaz@kristan-sp.si>
2025-04-06 10:50:25 +0200

    WLEB-BB 0.15.1

--- wled00/wled.h
+++ wled00/wled.h
@@ -3,13 +3,13 @@
 /*
    Main sketch, global variable declarations
    @title WLED project sketch
-   @version 0.15.1-b1
+   @version 0.15.1
    @author Christian Schwinne
  */
 
 // version code in format yymmddb (b = daily build)
 #ifndef AUTOBUILD
-#define VERSION 2504040
+#define VERSION 2504060
 #else
 #define VERSION BUILD
 #endif
