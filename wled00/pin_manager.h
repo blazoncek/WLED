aad5d90fe283ee1735649e48bb2d5c68c42cac0a
Blaž Kristan <blaz@kristan-sp.si>
2025-11-11 12:32:48 +0100

    Enhance PinManager allocateMultiplePins()

--- wled00/pin_manager.h
+++ wled00/pin_manager.h
@@ -103,7 +103,8 @@ namespace PinManager {
   // Provided to simplify error condition handling in clients
   // using more than one pin, such as I2C, SPI, rotary encoders,
   // ethernet, etc..
-  bool allocateMultiplePins(const managed_pin_type * mptArray, byte arrayElementCount, PinOwner tag );
+  bool allocateMultiplePins(const managed_pin_type * mptArray, byte arrayElementCount, PinOwner tag);
+  bool allocateMultiplePins(const int8_t * mptArray, byte arrayElementCount, PinOwner tag, bool output);
 
   [[deprecated("Replaced by three-parameter allocatePin(gpio, output, ownerTag), for improved debugging")]]
   inline bool allocatePin(byte gpio, bool output = true) { return allocatePin(gpio, output, PinOwner::None); }
