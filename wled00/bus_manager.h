f5e1ef706e2157d452c03de1be20b789708cbe15
Blaž Kristan <blaz@kristan-sp.si>
2026-03-03 11:47:29 +0100

    POC WS281x RGB+CCT support (dual chip)

--- wled00/bus_manager.h
+++ wled00/bus_manager.h
@@ -171,14 +171,14 @@ class Bus {
       return !((type >= TYPE_WS2812_1CH && type <= TYPE_WS2812_WWA) || type == TYPE_ANALOG_1CH || type == TYPE_ANALOG_2CH || type == TYPE_ONOFF);
     }
     static constexpr bool hasWhite(uint8_t type) {
-      return  (type >= TYPE_WS2812_1CH && type <= TYPE_WS2812_WWA) ||
+      return  (type >= TYPE_WS2812_1CH && type <= TYPE_WS2812_WWA) || type == TYPE_WS281X_DUAL || type == TYPE_WS281X_WWCW ||
               type == TYPE_SK6812_RGBW || type == TYPE_TM1814 || type == TYPE_UCS8904 ||
               type == TYPE_FW1906 || type == TYPE_WS2805 || type == TYPE_SM16825 ||        // digital types with white channel
               (type > TYPE_ONOFF && type <= TYPE_ANALOG_5CH && type != TYPE_ANALOG_3CH) || // analog types with white channel
               type == TYPE_NET_DDP_RGBW || type == TYPE_NET_ARTNET_RGBW;                   // network types with white channel
     }
     static constexpr bool hasCCT(uint8_t type) {
-      return  type == TYPE_WS2812_2CH_X3 || type == TYPE_WS2812_WWA ||
+      return  type == TYPE_WS2812_2CH_X3 || type == TYPE_WS2812_WWA || type == TYPE_WS281X_WWCW ||
               type == TYPE_ANALOG_2CH    || type == TYPE_ANALOG_5CH ||
               type == TYPE_FW1906        || type == TYPE_WS2805     ||
               type == TYPE_SM16825;
